10c117586ba3080340e437e85cd81525b2915663
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2022-06-27 19:31:52 -0400

    updated database.js and server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a5ff2b9aac7174b8ab72084ae791225e'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
